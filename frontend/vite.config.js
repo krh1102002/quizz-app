a9383c339d1a3ed0d6b630fd5a1b8aec15dfbcf5
krh1102002 <hulke1102002@gmail.com>
2025-12-29 11:09:22 +0530

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://quizz-app-2-bn5d.onrender.com',
         changeOrigin: true
       }
     }
